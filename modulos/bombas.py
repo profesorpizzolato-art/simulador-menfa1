c836ec4e6fd296b79329478a370026f53dd0dbb7
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:23:09 -0300

    Create bombas.py

---
+++ modulos/bombas.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+def bombas():
+
+    st.subheader("Sistema de bombas")
+
+    for i in range(1,4):
+
+        st.write("Bomba",i)
+
+        estado = st.selectbox(
+        f"Estado bomba {i}",
+        ["OFF","ON"]
+        )
+
+        if estado == "ON":
+
+            spm = st.slider(
+            f"SPM bomba {i}",
+            0,200,110
+            )
+
+            presion = st.slider(
+            f"Presión bomba {i}",
+            0,5000,3000
+            )
+
+            st.metric("SPM",spm)
+            st.metric("Presión",presion)
+
+        else:
+
+            st.warning("Bomba apagada")
