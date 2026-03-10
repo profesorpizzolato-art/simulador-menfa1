e89d2d6c4570c1d94d010ca5eeabc3d2aeebd1cf
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:06:35 -0300

    Create preguntas.py

---
+++ preguntas.py
@@ -0,0 +1,26 @@
+preguntas = [
+
+{
+"pregunta":"¿Que variable indica la velocidad de penetración?",
+"opciones":["ROP","RPM","Torque","SPP"],
+"correcta":"ROP"
+},
+
+{
+"pregunta":"¿Qué parámetro controla el peso sobre la barrena?",
+"opciones":["WOB","SPM","Flow","Torque"],
+"correcta":"WOB"
+},
+
+{
+"pregunta":"¿Qué indica aumento de torque?",
+"opciones":[
+"Fricción en el pozo",
+"Mayor presión",
+"Menor ROP",
+"Ninguna"
+],
+"correcta":"Fricción en el pozo"
+}
+
+]
