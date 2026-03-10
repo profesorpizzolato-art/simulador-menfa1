6dbafa550467214a82b27825e3a269a8b8d96bea
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:28:16 -0300

    Create preguntas.py

---
+++ evaluacion/preguntas.py
@@ -0,0 +1,18 @@
+preguntas = []
+
+for i in range(60):
+
+    preguntas.append({
+
+    "pregunta": f"Pregunta técnica de perforación {i+1}",
+
+    "opciones":[
+    "Opción A",
+    "Opción B",
+    "Opción C",
+    "Opción D"
+    ],
+
+    "correcta":"Opción A"
+
+    })
