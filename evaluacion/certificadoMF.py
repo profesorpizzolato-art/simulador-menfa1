b1eef5f14d2fbcc82a80cd8d5e474bd26307fd2c
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:31:14 -0300

    Create certificadoMF.py

---
+++ evaluacion/certificadoMF.py
@@ -0,0 +1,29 @@
+from PIL import Image,ImageDraw
+import streamlit as st
+
+def certificado(nombre,puntaje):
+
+    img = Image.new("RGB",(1200,800),"white")
+
+    draw = ImageDraw.Draw(img)
+
+    texto = f"""
+
+CERTIFICADO MENFA
+
+Se certifica que
+
+{nombre}
+
+Aprobó el simulador operativo
+de perforación petrolera
+
+Resultado: {puntaje}/60
+
+"""
+
+    draw.text((200,300),texto,fill="black")
+
+    img.save("certificado.png")
+
+    st.image("certificado.png")
