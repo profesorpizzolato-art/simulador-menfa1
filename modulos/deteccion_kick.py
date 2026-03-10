f75d18c61492dc99832492e1d040e0243a97a99b
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:26:40 -0300

    Create deteccion_kick.py

---
+++ modulos/deteccion_kick.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def deteccion_kick():
+
+    st.subheader("Detección de Kick")
+
+    flujo = st.slider("Flow out",0,150,100)
+
+    pit = st.slider("Pit volume",0,1500,900)
+
+    spp = st.slider("SPP",0,5000,3200)
+
+    if flujo > 115 and pit > 920:
+
+        st.error("KICK DETECTADO")
+
+    else:
+
+        st.success("Sistema estable")
