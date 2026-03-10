5f5c7f0131253ce2d3b10bf05e03b18cce60d5d4
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 19:22:30 -0300

    Create torque_drag.py

---
+++ modulos/modulos/torque_drag.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+def torque_drag():
+
+    st.subheader("Torque y Drag")
+
+    profundidad = st.slider("Profundidad ft",1000,20000,8000)
+
+    friccion = st.slider("Coeficiente fricción",0.1,0.5,0.25)
+
+    peso = st.slider("Peso sarta klb",10,200,90)
+
+    torque = peso * friccion * profundidad/120
+
+    st.metric("Torque estimado",int(torque))
+
+    x = np.linspace(0,profundidad,100)
+    y = x * friccion
+
+    fig,ax = plt.subplots()
+
+    ax.plot(x,y)
+
+    ax.set_xlabel("Profundidad")
+    ax.set_ylabel("Torque relativo")
+
+    st.pyplot(fig)
+
+    if torque > 25000:
+
+        st.error("Riesgo de pega diferencial")
