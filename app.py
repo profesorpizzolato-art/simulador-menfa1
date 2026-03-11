941c84ed316c5dd73ec7f017998d37bf9863e972
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 23:43:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+# Importación de módulos
 from modulos.panel_perforador import panel_perforador
 from modulos.panel_cabina import panel_cabina
 from modulos.torque_drag import torque_drag
@@ -9,6 +10,7 @@ from modulos.geonavegacion import geonavegacion
 from evaluacion.examen import examen
 
 
+# Configuración de página
 st.set_page_config(
     page_title="Simulador MENFA",
     layout="wide",
@@ -16,36 +18,27 @@ st.set_page_config(
 )
 
 
+# Título principal
 st.title("SIMULADOR OPERATIVO DE PERFORACIÓN MENFA")
 
 
+# Diccionario de módulos (más limpio que muchos elif)
+modulos = {
+    "Panel del Perforador": panel_perforador,
+    "Cabina de perforación": panel_cabina,
+    "Torque y Drag": torque_drag,
+    "Bombas de Lodo": bombas_lodo,
+    "Geonavegación": geonavegacion,
+    "Evaluación": examen
+}
+
+
+# Menú lateral
 menu = st.sidebar.selectbox(
     "Seleccionar módulo",
-    [
-        "Panel del Perforador",
-        "Cabina de perforación",
-        "Torque y Drag",
-        "Bombas de Lodo",
-        "Geonavegación",
-        "Evaluación"
-    ]
+    list(modulos.keys())
 )
 
 
-if menu == "Panel del Perforador":
-    panel_perforador()
-
-elif menu == "Cabina de perforación":
-    panel_cabina()
-
-elif menu == "Torque y Drag":
-    torque_drag()
-
-elif menu == "Bombas de Lodo":
-    bombas_lodo()
-
-elif menu == "Geonavegación":
-    geonavegacion()
-
-elif menu == "Evaluación":
-    examen()
+# Ejecutar módulo seleccionado
+modulos[menu]()
