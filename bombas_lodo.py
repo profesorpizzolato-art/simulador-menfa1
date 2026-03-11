6d63bf77acdc5783061ca45bed1a57d4e85f4b04
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 22:25:00 -0300

    Update bombas_lodo.py

--- bombas_lodo.py
+++ bombas_lodo.py
@@ -1,27 +1,32 @@
 import streamlit as st
-from PIL import Image
 
-def panel_bombas():
+def bombas_lodo():
 
-    st.subheader("Bombas de Lodo")
-
-    imagen = Image.open("assets/bomba_lodo.png")
-    st.image(imagen,width=400)
+    st.header("Sistema de Bombas de Lodo")
 
     for i in range(1,4):
 
-        st.write("Bomba",i)
+        st.subheader(f"Bomba {i}")
 
         estado = st.selectbox(
-        "Estado",
-        ["OFF","ON"],
-        key=i
+            "Estado",
+            ["OFF","ON"],
+            key=f"estado{i}"
         )
 
         if estado == "ON":
 
-            spm = st.slider("SPM",0,200,120,key=f"spm{i}")
-            presion = st.slider("Presión psi",0,5000,3000,key=f"pres{i}")
+            spm = st.slider(
+                "SPM",
+                0,200,110,
+                key=f"spm{i}"
+            )
+
+            presion = st.slider(
+                "Presión psi",
+                0,5000,3000,
+                key=f"pres{i}"
+            )
 
             st.metric("SPM",spm)
             st.metric("Presión",presion)
