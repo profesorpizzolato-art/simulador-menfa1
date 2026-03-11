19c1d746814d3e7c506e42823c2f40b3eec0b227
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 22:07:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Simulador MENFA", layout="wide")
 
 # 3. Intentar importar módulos (con manejo de errores para diagnóstico)
 try:
-    from modulos.panel_perforador import panel_perforador
+    from "modulos.panel_perforador import panel_perforador"
     # Agrega aquí otros módulos si ya existen los archivos .py en la carpeta modulos
     # from modulos.torque_drag import torque_drag
 except ImportError as e:
