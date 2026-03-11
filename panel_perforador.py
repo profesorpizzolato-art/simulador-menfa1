a5f620913acca45011b4d2177a128cffce198a6d
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-10 22:23:08 -0300

    Update panel_perforador.py

--- panel_perforador.py
+++ panel_perforador.py
@@ -1,49 +1,34 @@
 import streamlit as st
-from PIL import Image
 
 def panel_perforador():
 
-    st.subheader("Cabina del Perforador")
+    st.header("Panel del Perforador")
 
-    imagen = Image.open("assets/rig_floor.png")
-    st.image(imagen)
-
-    col1,col2,col3 = st.columns(3)
+    col1,col2,col3,col4 = st.columns(4)
 
     with col1:
-        wob = st.slider("WOB klb",0,80,25)
-        rpm = st.slider("RPM",0,200,120)
+        wob = st.slider("WOB (klb)",0,80,30)
 
     with col2:
-        torque = st.slider("Torque lb-ft",0,40000,15000)
-        rop = st.slider("ROP ft/hr",0,200,45)
-import streamlit as st
-from PIL import Image
-
-def panel_perforador():
-
-    st.subheader("Cabina del Perforador")
-
-    imagen = Image.open("assets/rig_floor.png")
-    st.image(imagen)
-
-    col1,col2,col3 = st.columns(3)
-
-    with col1:
-        wob = st.slider("WOB klb",0,80,25)
-        rpm = st.slider("RPM",0,200,120)
-
-    with col2:
-        torque = st.slider("Torque lb-ft",0,40000,15000)
-        rop = st.slider("ROP ft/hr",0,200,45)
+        rpm = st.slider("RPM",0,220,120)
 
     with col3:
-        presion = st.slider("SPP psi",0,5000,3000)
+        torque = st.slider("Torque (lb-ft)",0,40000,15000)
+
+    with col4:
+        rop = st.slider("ROP (ft/hr)",0,200,40)
+
+    st.divider()
+
+    col5,col6,col7 = st.columns(3)
+
+    with col5:
+        spp = st.slider("SPP psi",0,5000,3200)
+
+    with col6:
         flow = st.slider("Flow gpm",0,1200,650)
 
-    st.success("Sistema operativo activo")
-    with col3:
-        presion = st.slider("SPP psi",0,5000,3000)
-        flow = st.slider("Flow gpm",0,1200,650)
+    with col7:
+        pit = st.slider("Pit Volume bbl",0,1500,900)
 
-    st.success("Sistema operativo activo")
+    st.success("Sistema de perforación operativo")
